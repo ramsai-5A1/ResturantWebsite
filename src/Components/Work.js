32352e7542cdebc37dd2549233d793546ddd0628
Ramsai <ramsaicrony@gmail.com>
2023-12-16 18:52:31 +0530

    Website frontend complete

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -20,9 +20,29 @@ const Work = () => {
             title: "Fast Deliveries",
             text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum"
         }
-    ]
-    return <div>
-
+    ];
+    return <div className="work-section-wrapper">
+        <div className="work-section-top">
+            <p className="primary-subheading">Work</p>
+            <h1 className="primary-heading">How It Works</h1>
+            <p className="primary-text">
+                Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+            elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+            </p>
+        </div>
+        <div className="work-section-bottom">
+            {
+                workInfoData.map((data) => (
+                    <div className="work-section-info">
+                        <div className="info-boxes-img-container">
+                            <img src={data.image} alt="" />
+                        </div>
+                        <h2>{data.title}</h2>
+                        <p>{data.text}</p>
+                    </div>
+                ))
+            }
+        </div>
     </div>
 }
 
